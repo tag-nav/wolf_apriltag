12b0ce6b21aa63d5267e521ed2a4f4380fa5ace2
Joan Sola <jsola@iri.upc.edu>
2021-12-24 22:59:04 +0100

    Add reference to original IPPE work and code

--- include/apriltag/processor/ippe.h
+++ include/apriltag/processor/ippe.h
@@ -19,6 +19,22 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //--------LICENSE_END--------
+
+// This is the core header file for IPPE. Infinitesimal Plane-based Pose Estimation (IPPE) is a very fast and accurate
+// way to compute a camera's pose from a single image of a planar object using point correspondences. This has uses in
+// several applications, including augmented reality, 3D tracking and pose estimation with planar markers, and 3D scene
+// understanding.
+// This package is free and covered by the BSD licence without any warranty. We hope you find this code useful and if so
+// please cite our paper in your work:
+
+//@article{ year={2014}, issn={0920-5691}, journal={International Journal of Computer Vision}, volume={109}, number={3},
+//doi={10.1007/s11263-014-0725-5}, title={Infinitesimal Plane-Based Pose Estimation},
+//url={http://dx.doi.org/10.1007/s11263-014-0725-5}, publisher={Springer US}, keywords={Plane; Pose; SfM; PnP;
+//Homography}, author={Collins, Toby and Bartoli, Adrien}, pages={252-286}, language={English} }
+
+// Please contact Toby (toby.collins@gmail.com) if you have any questions about the code, paper and IPPE.
+
+
 #ifndef _IPPE_H_
 #define _IPPE_H_
 
